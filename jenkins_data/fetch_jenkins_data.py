7c9eb1e09892546eb894bf0c5877e77d14230962
Hung Nguyen <hung.nguyen@student.tuni.fi>
2020-03-07 10:24:36 +0200

    Add functionality to detect whether a project contains many jobs

--- jenkins_data/fetch_jenkins_data.py
+++ jenkins_data/fetch_jenkins_data.py
@@ -1,23 +1,38 @@
 import sys
 import jenkins
 
+def get_jobs(path, server):
+    jobs = []
+    with open(path, 'r') as f:
+        for line in f:
+            project_name = line.strip()
+
+            # If the project contains many jobs, the name is a view
+            if server.view_exists(project_name) and not server.get_job_name(project_name):
+                server.get_jobs(folder_depth=0, view_name= project_name)
+
+            # The project itself is a job
+            else:
+                jobs.append(project_name)
+
+    return jobs
+
+def process_job(job, server):
+    pass
+
 if __name__ == "__main__":
 
     server = jenkins.Jenkins('https://builds.apache.org/')
 
-    try:
-        # Sometimes connecting to Jenkins server is banned due to ill use of API
-        # Test connection to server
-        print(f"Jenkins-API version: {server.get_version()}")
+    # Sometimes connecting to Jenkins server is banned due to ill use of API
+    # Test connection to server
+    print(f"Jenkins-API version: {server.get_version()}")
 
-
-    except OSError:
-        print("Error connecting to server")
+    if len(sys.argv != 2):
+        print("Provide also path to one csv file with projects' names in it.") 
         sys.exit(1)
 
-    if len(sys.argv != 1):
-        print("Provide path to one csv file with projects' names in it.") 
-        sys.exit(1)
+    jobs = get_jobs(sys.argv[1], server)    
 
-    try:
-        
+    for job in jobs:
+        process_job(job, server)
