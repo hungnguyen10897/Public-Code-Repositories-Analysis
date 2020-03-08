f2837738bbc834a1f5eacb22583e0287e1b0515f
Hung Nguyen <hung.nguyen@student.tuni.fi>
2020-03-08 09:33:48 +0200

    Use regex now to find job info

--- jenkins_data/fetch_jenkins_data.py
+++ jenkins_data/fetch_jenkins_data.py
@@ -1,24 +1,32 @@
 import sys
 import jenkins
+import re
 
-def get_jobs(path, server):
-    jobs = []
+def get_projects(path):
+    projects = []
     with open(path, 'r') as f:
         for line in f:
             project_name = line.strip()
+            projects.append(project_name)
 
-            # If the project contains many jobs, the name is a view
-            if server.view_exists(project_name) and not server.get_job_name(project_name):
-                server.get_jobs(folder_depth=0, view_name= project_name)
+    return projects
 
-            # The project itself is a job
-            else:
-                jobs.append(project_name)
+def process_project(project, server):
+    regex = re.compile(f"^.*{project}.*$", re.IGNORECASE)
 
-    return jobs
+    jobs_info = server.get_job_info_regex(regex, folder_depth=0, depth=0)
 
-def process_job(job, server):
-    pass
+    for job_info in jobs_info:
+        class_ = job_info['_class'].split('.')[-1]
+        name = job_info['fullName']
+
+        if class_ in ['Folder', 'Folder', 'OrganizationFolder', 'WorkflowMultiBranchProject']:
+            #There should be a "jobs" field containing more jobs:
+            pass
+
+        else:
+            builds_data = job_info['builds']
+
 
 if __name__ == "__main__":
 
@@ -28,11 +36,12 @@ if __name__ == "__main__":
     # Test connection to server
     print(f"Jenkins-API version: {server.get_version()}")
 
-    if len(sys.argv != 2):
+    if len(sys.argv)  != 2:
         print("Provide also path to one csv file with projects' names in it.") 
         sys.exit(1)
 
-    jobs = get_jobs(sys.argv[1], server)    
+    projects = get_projects(sys.argv[1])    
+    print(projects)
 
-    for job in jobs:
-        process_job(job, server)
+    # for project in projects:
+    #     process_project(project, server)
