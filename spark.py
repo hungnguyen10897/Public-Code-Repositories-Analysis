287a45a670190fcfd5c05c678f7cfdebc7111d94
hungnguyen10897 <hungnguyen10897@gmail.com>
2020-03-18 17:00:43 +0200

    Finish Loading from two datasources and merge into one df for ML

--- spark.py
+++ spark.py
@@ -1,4 +1,5 @@
 # pyspark --conf spark.executor.extraClassPath=sqlite-jdbc-3.30.1.jar --driver-class-path sqlite-jdbc-3.30.1.jar --jars sqlite-jdbc-3.30.1.jar
+# spark-submit --conf spark.executor.extraClassPath=sqlite-jdbc-3.30.1.jar --driver-class-path sqlite-jdbc-3.30.1.jar --jars sqlite-jdbc-3.30.1.jar spark.py
 from pyspark.sql import SparkSession
 from pathlib import Path
 from pyspark.sql import DataFrame
@@ -40,20 +41,37 @@ def get_jenkins_builds_data(jenkins_data_directory):
             StructField("test_fail_count", IntegerType(), True),
             StructField("test_skip_count", IntegerType(), True),
             StructField("total_test_duration", DoubleType(), True)]
-
     schema = StructType(field)
-
     output = spark.createDataFrame(sc.emptyRDD(), schema)
-
     for file in builds_path.glob('*.csv'):
-        df = spark.read.csv(file, sep=',', schema = schema, ignoreLeadingWhiteSpace = True, 
+        print(file.resolve())
+
+        df = spark.read.csv(str(file.resolve()), sep=',', schema = schema, ignoreLeadingWhiteSpace = True, 
             ignoreTrailingWhiteSpace = True, header=True)
 
-        output.union(df)
-
-    output.withColumn('commit_ts', )
-
+        output = output.union(df)
     return output
+
+def refactor_type_count(refactoring_miner_df):
+
+    rows = refactoring_miner_df.select("refactoringType").distinct().collect()
+    select_statement = []
+    for row in rows:
+        type = row.refactoringType
+        comparison_str = "\'" + row.refactoringType + "\'"
+        field_name = type.replace(' ','_') + "_Count"
+        sum_str = f"SUM(case refactoringType when {comparison_str} then 1 else 0 end) AS {field_name}"
+        select_statement.append(sum_str)
+    
+    sql_str = f"""
+        SELECT 
+            projectID,
+            commitHash,
+            {",".join(select_statement)}
+        FROM REFACTORING_MINER GROUP BY projectID,commitHash
+    """
+
+    return spark.sql(sql_str)
 
 if __name__ == "__main__":
 
@@ -62,8 +80,23 @@ if __name__ == "__main__":
 
     db = retrieve_sqlite(sqlite_conn_url)
     projects_df = db['PROJECTS']
+    refactoring_miner_df = db['REFACTORING_MINER']
+
+    refactoring_miner_df = refactoring_miner_df.filter("refactoringType IS NOT NULL")
+    refactoring_miner_df.persist()
+    refactoring_miner_df.createOrReplaceTempView("REFACTORING_MINER")
+
+    refactor_count_df = refactor_type_count(refactoring_miner_df)
+    refactor_count_df.persist()
+    refactoring_miner_df.unpersist()
 
     jenkins_builds_df = get_jenkins_builds_data(jenkins_data_directory)
     jenkins_builds_df.show()
 
+    jenkins_builds_df = jenkins_builds_df.filter("commit_id IS NOT NULL")
 
+
+    result = jenkins_builds_df.join(refactor_count_df, jenkins_builds_df.commit_id == refactor_count_df.commitHash, how = 'inner')
+    result.cache()
+
+    spark.stop()
