0d38a4639a95a3b809bef4eeea6028fc4eee3ab7
hungnguyen10897 <hungnguyen10897@gmail.com>
2020-03-27 18:27:29 +0200

    Function to extract all metrics info from Sonar, Merge with extraction of projects' info into one function

--- sonarcloud_data/fetch_sonarcloud_data.py
+++ sonarcloud_data/fetch_sonarcloud_data.py
@@ -2,37 +2,70 @@ import requests
 import math
 
 SERVER = "https://sonarcloud.io/"
+ORGANIZATION = "apache"
 
-def get_all_projects():
-    page_size = 200
-    params = {'organization':'apache', 'qualifiers': 'TRK', 'p':1, 'ps': page_size}
+def get_server_info(type, iter = 1):
 
-    r = requests.get(SERVER + "api/components/search", params=params)
+    page_size = 500
+    params = {'p' : iter, 'ps':page_size}
+    if type == 'projects':
+        endpoint = SERVER + "api/components/search"
+        params['organization'] = ORGANIZATION
+        params['qualifiers'] = 'TRK'
+
+    elif type == 'metrics':
+        endpoint = SERVER + "api/metrics/search"
+
+    else:
+        print("ERROR: Illegal info type.")
+        return None
+
+    r = requests.get(endpoint, params=params)
 
     if r.status_code != 200:
         print(f"ERROR: HTTP Response code {r.status_code} for request {r.request.path_url}")
-    else:
-        r_dict = r.json()
-    
-    project_list = r_dict['components']
-    
-    total_num_projects = r_dict['paging']['total']
-    iter_num = math.ceil(total_num_projects/page_size)
+        return None
 
-    for i in range(2,iter_num + 1):
-        params['p'] = i
-        r = requests.get(SERVER + "api/components/search", params=params)
-        if r.status_code != 200:
-            print(f"ERROR: HTTP Response code {r.status_code} for request {r.request.path_url}")
-        else:
-            r_dict = r.json()
-            project_list = project_list + r_dict['components']
+    r_dict = r.json()
+
+    if type == 'projects':
+        element_list = r_dict['components']
+        total_num_elements = r_dict['paging']['total']
+    elif type == 'metrics':
+        element_list = r_dict['metrics']
+        total_num_elements = r_dict['total']
+
+    if iter*page_size < total_num_elements:
+        element_list = element_list + get_server_info(type, iter+1)
     
-    return project_list
+    return element_list
+
+def process_project(project):
+    page_size = 200
+    params = {}
+    key = project['key']
+
+    r = requests.get(SERVER + "api/project_analyses/search")
+
 
 if __name__ == "__main__":
 
-    project_list = get_all_projects()
-    
+    # metric_list = get_server_info(type='metrics')
+    # metric_list.sort(key = lambda x: ('None' if 'domain' not in x else x['domain'], int(x['id'])))
+
+    # # Write metrics to a file
+    # with open('./metrics.txt', 'w') as f:
+    #     for metric in metric_list:
+    #         f.write("{} - {} - {} - {}\n".format(
+    #             'No ID' if 'id' not in metric else metric['id'],
+    #             'No Domain' if 'domain' not in metric else metric['domain'],
+    #             'No Key' if 'key' not in metric else metric['key'],
+    #             'No Description' if 'description' not in metric else metric['description']
+    #             ))
+
+    project_list = get_server_info(type='projects')
+    project_list.sort(key = lambda x: x['key'])
+
     for project in project_list:
-        pass
+        process_project(project)
+
