bee0d77fd6a4c78b974dcf0671aebe2ffd79513d
hungnguyen10897 <hungnguyen10897@gmail.com>
2020-03-28 19:57:12 +0200

    Impose data type on measures

--- sonarcloud_data/fetch_sonarcloud_data.py
+++ sonarcloud_data/fetch_sonarcloud_data.py
@@ -95,22 +95,59 @@ def load_metrics(path = None):
         with open(p, 'r') as f:
             order = 0
             for line in f:
-                metric = line.split(' - ')[2]
-                metrics_order[metric] = order
+                parts = line.split(' - ')
+                metric = parts[2]
+                type = parts[3]
+                metrics_order[metric] = (order,type)
                 order += 1
         return metrics_order
     except:
         print("ERROR: Reading metrics file")
         sys.exit(1)
 
-def extract_measures_value(measures):
+def safe_cast(val, to_type):
+    if to_type in ['INT' ,'WORK_DUR']:
+        try:
+            return int(val)
+        except (ValueError, TypeError):
+            print(f"ERROR: error casting value {str(val)} to type {to_type}")
+            return None
+    elif to_type in ['FLOAT', 'PERCENT', 'RATING']:
+        try:
+            return float(val)
+        except (ValueError, TypeError):
+            print(f"ERROR: error casting value {str(val)} to type {to_type}")
+            return None
+    elif to_type == 'BOOL':
+        try:
+            return bool(val)
+        except (ValueError, TypeError):
+            print(f"ERROR: error casting value {str(val)} to type {to_type}")
+            return None
+    elif type == 'MILISEC':
+        try:
+            return datetime.fromtimestamp(int(val)/1000)
+
+    else:
+        try:
+            return str(val)
+        except (ValueError, TypeError):
+            print(f"ERROR: error casting to type {to_type}")
+            return None
+
+
+
+def extract_measures_value(measures, metrics_order_type):
     columns = []
     data = {}
     for measure in measures:
         metric = measure['metric']
+
+        type = metrics_order_type[metric][1]
+
         columns.append(metric)
         history = measure['history']
-        values = list((map(lambda x: None if 'value' not in x else x['value'], history)))
+        values = list((map(lambda x: None if 'value' not in x else safe_cast(x['value'],type), history)))
         data[metric] = values
     
     return columns, data
@@ -131,40 +168,41 @@ def process_project(project, metrics_path = None):
         version = None if 'projectVersion' not in analysis else analysis['projectVersion']
         version_list.append(version)
     
-    metrics_order = load_metrics(metrics_path)
-    metrics = list(metrics_order.keys())
+    metrics_order_type = load_metrics(metrics_path)
+    metrics = list(metrics_order_type.keys())
 
     measures = []
     for i in range(0,len(metrics), 15):
         #Get measures
         measures = measures + query_server('measures',1,project_key, metrics[i:i+15])
     
-    measures.sort(key = lambda x: metrics_order[x['metric']])
+    measures.sort(key = lambda x: metrics_order_type[x['metric']][0])
 
     # For testing
     # return measures
 
-    metric_columns, measures_data = extract_measures_value(measures)
+    metric_columns, measures_data = extract_measures_value(measures, metrics_order_type)
     #Create DF
     df = pd.DataFrame(measures_data,columns= metric_columns)
     print(0)
 
+def write_metrics_file(metric_list):
+    metric_list.sort(key = lambda x: ('None' if 'domain' not in x else x['domain'], int(x['id'])))
+
+    with open('./all_metrics.txt', 'w') as f:
+        for metric in metric_list:
+            f.write("{} - {} - {} - {} - {}\n".format(
+                'No ID' if 'id' not in metric else metric['id'],
+                'No Domain' if 'domain' not in metric else metric['domain'],
+                'No Key' if 'key' not in metric else metric['key'],
+                'No Type' if 'type' not in metric else metric['type'],
+                'No Description' if 'description' not in metric else metric['description']
+                ))
 
 if __name__ == "__main__":
 
-    # metric_list = query_server(type='metrics')
-    # metric_list.sort(key = lambda x: ('None' if 'domain' not in x else x['domain'], int(x['id'])))
-
-    # # Write all metrics to a file
-    # with open('./all_metrics.txt', 'w') as f:
-    #     for metric in metric_list:
-    #         f.write("{} - {} - {} - {}\n".format(
-    #             'No ID' if 'id' not in metric else metric['id'],
-    #             'No Domain' if 'domain' not in metric else metric['domain'],
-    #             'No Key' if 'key' not in metric else metric['key'],
-    #             'No Description' if 'description' not in metric else metric['description']
-    #             ))
-
+    # Write all metrics to a file
+    # write_metrics_file(query_server(type='metrics'))
 
     project_list = query_server(type='projects')
     project_list.sort(key = lambda x: x['key'])
