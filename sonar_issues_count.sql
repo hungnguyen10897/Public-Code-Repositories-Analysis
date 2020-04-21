97c8eb20bf3434579d234f6a60f499d1c2f27012
hungnguyen10897 <hungnguyen10897@gmail.com>
2020-04-21 16:17:27 +0300

    Fix a bug in sonar_issues_count.sql query that introduces all-zero data rows

--- sonar_issues_count.sql
+++ sonar_issues_count.sql
@@ -62,78 +62,83 @@ coalesce(removed_null_severity_security_hotspot, 0) AS removed_null_severity_sec
 
 FROM
 (
-
 SELECT 
 project,
 creation_analysis_key,
 MAX(creation_date) as creation_date,
 
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'BLOCKER' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_blocker_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'CRITICAL' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_critical_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MAJOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_major_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MINOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_minor_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'INFO' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_info_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity IS NULL AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_null_severity_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS introduced_blocker_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'CRITICAL' THEN 1 ELSE 0 END) AS introduced_critical_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS introduced_major_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS introduced_minor_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'INFO' THEN 1 ELSE 0 END) AS introduced_info_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity IS NULL THEN 1 ELSE 0 END) AS introduced_null_severity_code_smell,
 
-SUM(CASE WHEN type = 'BUG' AND severity = 'BLOCKER' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_blocker_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'CRITICAL' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_critical_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'MAJOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_major_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'MINOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_minor_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'INFO' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_info_bug,
-SUM(CASE WHEN type = 'BUG' AND severity IS NULL AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_null_severity_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS introduced_blocker_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'CRITICAL' THEN 1 ELSE 0 END) AS introduced_critical_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS introduced_major_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS introduced_minor_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'INFO' THEN 1 ELSE 0 END) AS introduced_info_bug,
+SUM(CASE WHEN type = 'BUG' AND severity IS NULL THEN 1 ELSE 0 END) AS introduced_null_severity_bug,
 
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'BLOCKER' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_blocker_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'CRITICAL' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_critical_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MAJOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_major_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MINOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_minor_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'INFO' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_info_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity IS NULL AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_null_severity_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS introduced_blocker_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'CRITICAL' THEN 1 ELSE 0 END) AS introduced_critical_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS introduced_major_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS introduced_minor_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'INFO' THEN 1 ELSE 0 END) AS introduced_info_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity IS NULL THEN 1 ELSE 0 END) AS introduced_null_severity_vulnerability,
 
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'BLOCKER' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_blocker_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'CRITICAL' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_critical_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MAJOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_major_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MINOR' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_minor_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'INFO' AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_info_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity IS NULL AND status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW') THEN 1 ELSE 0 END) AS introduced_null_severity_security_hotspot
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS introduced_blocker_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'CRITICAL' THEN 1 ELSE 0 END) AS introduced_critical_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS introduced_major_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS introduced_minor_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'INFO' THEN 1 ELSE 0 END) AS introduced_info_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity IS NULL THEN 1 ELSE 0 END) AS introduced_null_severity_security_hotspot
 
-from sonar_issues GROUP BY project, creation_analysis_key) t1
+from sonar_issues 
+WHERE status IN ('OPEN', 'REOPENED', 'CONFIRMED', 'TO_REVIEW')
+GROUP BY project, creation_analysis_key
+) t1
 
 FULL OUTER JOIN
 
-(SELECT 
+(
+SELECT 
 project,
 current_analysis_key,
 MAX(update_date) as update_date,
 
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'BLOCKER' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_blocker_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'CRITICAL' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_critical_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MAJOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_major_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MINOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_minor_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'INFO' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_info_code_smell,
-SUM(CASE WHEN type = 'CODE_SMELL' AND severity IS NULL AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_null_severity_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS removed_blocker_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'CRITICAL'  THEN 1 ELSE 0 END) AS removed_critical_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS removed_major_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS removed_minor_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity = 'INFO' THEN 1 ELSE 0 END) AS removed_info_code_smell,
+SUM(CASE WHEN type = 'CODE_SMELL' AND severity IS NULL THEN 1 ELSE 0 END) AS removed_null_severity_code_smell,
 
-SUM(CASE WHEN type = 'BUG' AND severity = 'BLOCKER' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_blocker_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'CRITICAL' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_critical_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'MAJOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_major_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'MINOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_minor_bug,
-SUM(CASE WHEN type = 'BUG' AND severity = 'INFO' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_info_bug,
-SUM(CASE WHEN type = 'BUG' AND severity IS NULL AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_null_severity_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS removed_blocker_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'CRITICAL' THEN 1 ELSE 0 END) AS removed_critical_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS removed_major_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS removed_minor_bug,
+SUM(CASE WHEN type = 'BUG' AND severity = 'INFO' THEN 1 ELSE 0 END) AS removed_info_bug,
+SUM(CASE WHEN type = 'BUG' AND severity IS NULL THEN 1 ELSE 0 END) AS removed_null_severity_bug,
 
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'BLOCKER' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_blocker_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'CRITICAL' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_critical_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MAJOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_major_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MINOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_minor_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'INFO' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_info_vulnerability,
-SUM(CASE WHEN type = 'VULNERABILITY' AND severity IS NULL AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_null_severity_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS removed_blocker_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'CRITICAL' THEN 1 ELSE 0 END) AS removed_critical_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS removed_major_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS removed_minor_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity = 'INFO' THEN 1 ELSE 0 END) AS removed_info_vulnerability,
+SUM(CASE WHEN type = 'VULNERABILITY' AND severity IS NULL THEN 1 ELSE 0 END) AS removed_null_severity_vulnerability,
 
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'BLOCKER' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_blocker_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'CRITICAL' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_critical_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MAJOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_major_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MINOR' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_minor_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'INFO' AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_info_security_hotspot,
-SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity IS NULL AND status IN ('RESOLVED', 'CLOSED', 'REVIEWED') THEN 1 ELSE 0 END) AS removed_null_severity_security_hotspot
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'BLOCKER' THEN 1 ELSE 0 END) AS removed_blocker_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'CRITICAL' THEN 1 ELSE 0 END) AS removed_critical_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MAJOR' THEN 1 ELSE 0 END) AS removed_major_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'MINOR' THEN 1 ELSE 0 END) AS removed_minor_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity = 'INFO' THEN 1 ELSE 0 END) AS removed_info_security_hotspot,
+SUM(CASE WHEN type = 'SECURITY_HOTSPOT' AND severity IS NULL THEN 1 ELSE 0 END) AS removed_null_severity_security_hotspot
 
-from sonar_issues GROUP BY project, current_analysis_key
+from sonar_issues 
+WHERE status IN ('RESOLVED', 'CLOSED', 'REVIEWED')
+GROUP BY project, current_analysis_key
 ) t2
 
 ON t1.creation_analysis_key = t2.current_analysis_key
