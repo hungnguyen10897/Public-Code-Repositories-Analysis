ef7bdefd1943f2616eb1631d47c2c6f946511922
hungnguyen10897 <hungnguyen10897@gmail.com>
2020-07-15 14:05:15 +0300

    Add temporary helper script to unstage csv files

---
+++ jenkins_data/temp_unstage.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+def process(file_directory):
+
+    
+
+    if not file_directory.exists():
+        return
+
+    for file in file_directory.glob("*_staging.csv"):
+        archive_file = Path(str(file).replace("_staging", ""))
+        file.rename(archive_file)
+
+if __name__ == "__main__":
+    file_directory = Path("./data/builds")
+    process(file_directory)
